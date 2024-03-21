e79196f220be064658237260e8dfb9d5a5dfe4d6
Mihai005 <talumihai@gmail.com>
2024-03-21 21:45:15 +0200

    Revert "Merge branch 'master' of https://github.com/Mihai005/OOP"
    This reverts commit 6809d16b8f2905eb482071c574d6a82c95f119e4, reversing
    changes made to ebee26941c646007bc18c96fdd451341d0b44818.

--- Domain.h
+++ Domain.h
@@ -8,15 +8,7 @@ typedef struct
     int concentration;
 }Medicine;
 
-Medicine* create_medicine(char* name, int price, int quantity, int concentration);
-void destroy_medicine(Medicine* medicine);
-char* get_name(Medicine* medicine);
-int get_price(Medicine* medicine);
-int get_quantity(Medicine* medicine);
-int get_concentration(Medicine* medicine);
-void set_name(Medicine* medicine, char* new_name);
-void set_price(Medicine* medicine, int new_price);
-void set_quantity(Medicine* medicine, int new_quantity);
-void set_concentration(Medicine* medicine, int new_concentration);
-
-void testdomain();
+char get_name(Medicine medicine);
+int get_price(Medicine medicine);
+int get_quantity(Medicine medicine);
+int get_concentration(Medicine medicine);
